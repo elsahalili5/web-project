366ea043b738eda55cab3cd40de2ecafe3f1082c
elsahalili5 <elsahalilii5@gmail.com>
2025-12-14 02:11:51 +0100

    ⚡️ add donation form validation

--- js/donate.js
+++ js/donate.js
@@ -1,71 +1,136 @@
-const form = document.querySelector(".payment-form");
-const resultDiv = document.getElementById("resultDiv");
-const resultP = document.getElementById("resultP");
+const donationForm = document.getElementById("donationForm");
+const resultDiv = document.querySelector(".result");
+const amountresultDiv = document.querySelector(".amount-result");
 
+const name_regex = /^[A-Za-z]+$/;
 const email_regex =
   /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
 
-form.addEventListener("submit", function (e) {
+donationForm.addEventListener("submit", function (e) {
   e.preventDefault();
+  const amountInput = document.getElementById("amountInput");
+  const emailInput = document.querySelector(
+    'input[placeholder="Email address"]'
+  );
+  const firstNameInput = document.querySelector(
+    'input[placeholder="First name"]'
+  );
+  const lastNameInput = document.querySelector(
+    'input[placeholder="Last name"]'
+  );
+  const cardNumberInput = document.querySelector(
+    'input[placeholder="Card number"]'
+  );
+  const expireDateInput = document.querySelector(
+    'input[placeholder="MM / YY"]'
+  );
+  const cvcInput = document.querySelector('input[placeholder="CVV"]');
+  const cardNameInput = document.querySelector(
+    'input[placeholder="Name on card"]'
+  );
+  const postalCodeInput = document.querySelector(
+    'input[placeholder="Postal code"]'
+  );
 
-  const name = document.getElementById("name").value.trim();
-  const email = document.getElementById("email").value.trim();
-  const phone = document.getElementById("phone").value.trim();
-  const address = document.getElementById("address").value.trim();
-  const cardNumber = document.getElementById("cardNumber").value.trim();
-  const expireDate = document.getElementById("expireDate").value.trim();
-  const cvc = document.getElementById("cvc").value.trim();
-  const amount = document.getElementById("amount").value.trim();
-
-  resultP.innerText = "";
   resultDiv.style.color = "red";
-  resultDiv.style.visibility = "visible";
+  resultDiv.innerText = "";
 
-  if (!name) {
-    resultP.innerText = "Enter Name*";
+  amountresultDiv.style.color = "red";
+  amountresultDiv.innerText = "";
+
+  if (!amountInput.value) {
+    amountresultDiv.innerText = "Enter Amount*";
+    amountresultDiv.style.visibility = "visible";
+    return false;
+  } else if (isNaN(amountInput.value) || Number(amountInput.value) <= 0) {
+    amountresultDiv.innerText = "Enter a valid Amount*";
+    amountresultDiv.style.visibility = "visible";
+
     return false;
   }
 
-  if (!email) {
-    resultP.innerText = "Enter Email*";
+  if (!emailInput.value.trim()) {
+    resultDiv.innerText = "Enter Email*";
+    resultDiv.style.visibility = "visible";
     return false;
-  } else if (!email_regex.test(email)) {
-    resultP.innerText = "Enter a valid Email*";
+  } else if (!email_regex.test(emailInput.value.trim())) {
+    resultDiv.innerText = "Enter a valid Email*";
+    resultDiv.style.visibility = "visible";
+
+    return false;
+  }
+  if (!firstNameInput.value.trim()) {
+    resultDiv.innerText = "Enter First Name*";
+    resultDiv.style.visibility = "visible";
+    return false;
+  } else if (!name_regex.test(firstNameInput.value)) {
+    resultDiv.innerText = "Enter a valid First Name*";
+    resultDiv.style.visibility = "visible";
+
+    return false;
+  }
+  if (!lastNameInput.value.trim()) {
+    resultDiv.innerText = "Enter Last Name*";
+    resultDiv.style.visibility = "visible";
+    return false;
+  } else if (!name_regex.test(lastNameInput.value)) {
+    resultDiv.innerText = "Enter a valid Last Name*";
+    resultDiv.style.visibility = "visible";
+
     return false;
   }
 
-  if (!phone) {
-    resultP.innerText = "Enter Phone*";
+  if (!cardNumberInput.value.trim()) {
+    resultDiv.innerText = "Enter Card Number*";
+    resultDiv.style.visibility = "visible";
+
     return false;
   }
 
-  if (!address) {
-    resultP.innerText = "Enter Address*";
+  if (!expireDateInput.value.trim()) {
+    resultDiv.innerText = "Enter Expire Date*";
+    resultDiv.style.visibility = "visible";
+
     return false;
   }
 
-  if (!cardNumber) {
-    resultP.innerText = "Enter Card Number*";
+  if (!cvcInput.value.trim()) {
+    resultDiv.innerText = "Enter CVC*";
+    resultDiv.style.visibility = "visible";
+
     return false;
   }
 
-  if (!expireDate) {
-    resultP.innerText = "Enter Expire Date*";
+  if (!cardNameInput.value.trim() || !name_regex.test(cardNameInput.value)) {
+    resultDiv.innerText = "Enter a valid Name on Card*";
+    resultDiv.style.visibility = "visible";
+
     return false;
   }
 
-  if (!cvc) {
-    resultP.innerText = "Enter CVC*";
+  if (!postalCodeInput.value.trim()) {
+    resultDiv.innerText = "Enter Postal Code*";
+    resultDiv.style.visibility = "visible";
+
     return false;
   }
 
-  if (!amount) {
-    resultP.innerText = "Enter Amount*";
-    return false;
-  } else if (isNaN(amount) || Number(amount) <= 0) {
-    resultP.innerText = "Enter a valid Amount*";
-    return false;
-  }
+  resultDiv.style.color = "green";
+  resultDiv.innerText = "Donation successful! Redirecting...";
 
-  form.reset();
+  setTimeout(() => {
+    window.location.href = "thankyou.html";
+
+    amountInput.value = 0;
+    emailInput.value = "";
+    firstNameInput.value = "";
+    lastNameInput.value = "";
+    cardNumberInput.value = "";
+    expireDateInput.value = "";
+    cvcInput.value = "";
+    cardNameInput.value = "";
+    postalCodeInput.value = "";
+  }, 1000);
+
+  return false;
 });
